0c6fa8bdcf45829184318fd4086935c414863ae3
Dnomd343 <dnomd343@gmail.com>
2023-12-12 23:21:17 +0800

    feat: enhance boot process

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,12 @@
 
 set -e
 
+if [[ "$1" == "dig" ]] || [[ "$1" == "rndc" ]] || [[ "$1" == "nslookup" ]] || [[ "$1" == "nsupdate" ]] || [[ "$1" == "tsig-keygen" ]] || [[ "$1" == "ddns-confgen" ]] || [[ "$1" == "rndc-confgen" ]]; then
+  exec $@
+  echo "Failed to run command -> \`$@\`"
+  exit 1
+fi
+
 if [ ! -f "/var/bind/named.ca" ]; then
   cp /usr/share/dns-root-hints/named.root /var/bind/named.ca
 fi
@@ -11,4 +17,9 @@ if [ ! -f "/etc/bind/bind.keys" ]; then
 fi
 
 named-checkconf
-exec named -f -g
+
+if [ $# -eq 0 ]; then
+  exec named -g
+else
+  exec named $@
+fi
